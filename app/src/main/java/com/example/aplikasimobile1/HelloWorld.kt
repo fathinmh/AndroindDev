5453f48497e99d93048ccb1153c0b82a0a395ffa
fathinmh <fathinhilmi81@gmail.com>
2022-10-25 16:38:08 +0700

    ProgressLanjut

---
+++ app/src/main/java/com/example/aplikasimobile1/HelloWorld.kt
@@ -0,0 +1,50 @@
+package com.example.aplikasimobile1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class HelloWorld : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Column() {
+                Image(
+                    painter = painterResource(id = R.drawable.himatel),
+                    contentDescription = "Gambar"
+                )
+                Surface(
+                    color = Color.White,
+                    modifier = Modifier.fillMaxWidth()
+                ){
+                    Text(
+                        text = "Hello World Activity",
+                        fontSize = 24.sp,
+                        modifier = Modifier.padding(16.dp, 16.dp, 16.dp, 4.dp),
+                        color = Color.Blue
+                    )
+                }
+
+                Text(
+                    text = "Jalan tidak?",
+                    modifier = Modifier.padding(16.dp, 0.dp, 16.dp, 4.dp),
+                    color = Color.White
+                )
+            }
+
+        }
+    }
+}
